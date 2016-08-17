a3aab68c2a0886d52f3c5978086e792ddd024d97
Bill Luczyk <Bill.Luczyk@med.ge.com>
2016-08-17 16:42:49 -0500

    Initial revision.

---
+++ fun.cpp
@@ -0,0 +1,16 @@
+/*+
+*************************************************************************
+
+FILE NAME:  fun.cpp
+AUTHOR:  Bill Luczyk
+CREATION DATE:  17-Aug-2016
+NARRATIVE:  Programming is fun.
+
+	Copyright 2016  General Electric Company
+*************************************************************************
+-*/
+
+main()
+{
+  printf("Programming is fun.\n");
+}
